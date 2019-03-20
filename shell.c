9e13a022a632adab5c5036f97074fa9dd6dd3a6f
Devi Krishnan <devikrishnan67@gmail.com>
2019-03-20 11:18:47 +0530

    modified child process

--- shell.c
+++ shell.c
@@ -2,13 +2,14 @@
 #include<unistd.h>
 #include<sys/wait.h>
 #include<sys/types.h>
-#include<string.h> 
+#include<string.h>
+#include<stdlib.h> 
 #define MAXLINE 80 /* The maximum length command */
 int main(void){
 	char buffer[MAXLINE/2 + 1];
 	char *args[MAXLINE/2 + 1]; /* command line arguments */
 	int shouldrun = 1; /* flag to determine when to exit program */
-	char *token;
+	char *token,*command;
 	int count;
 	while (shouldrun){
 		printf("osh>");
@@ -20,13 +21,25 @@ int main(void){
         	pid=fork();
         	if(pid==0){
                 	token=strtok(buffer, " ");
-			count=0;
+			command=strdup(token);
+                        args[0]=strdup(command);
+			count=1;
 			while(token != NULL){
-				args[count++]=token;
+				args[count]=strdup(token);
+				count++;
 				token = strtok(NULL, " ");
 			}
-			execvp(args[0],args);	
-        	}
+			args[count]="\0";
+			if(strcmp(args[0], "exit") == 0){
+				int doexit = 1;
+				exit(1);
+  			}
+			int result =execvp(command,args);
+			if(result < 0){
+				printf("*** ERROR: exec failed\n");
+        			exit(1);
+      			}	
+		}
         	else if(pid>0){
                 	int a=50;
                 	wait(&a);
